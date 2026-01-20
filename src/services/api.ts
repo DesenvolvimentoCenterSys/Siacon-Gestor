6eb9b54606e4f29e7c14b32ad7e354912afcd070
DesenvolvimentoCenterSys <desenvolvimento.centersys@gmail.com>
2026-01-20 16:39:35 -0300

    fix

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,10 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL:  'http://localhost:5000',
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:5000',
   headers: {
     'Content-Type': 'application/json',
     Accept: 'application/json',
-    Customer:  'localhost',
+    Customer: process.env.NEXT_PUBLIC_CUSTOMER || 'localhost',
   },
 })
